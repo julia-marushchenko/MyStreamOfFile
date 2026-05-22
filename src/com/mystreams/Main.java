dfb1930d162b779582a12199246ec1e17582e4c5
Julia Marushchenko <marushchenko.yulia@gmail.com>
2026-05-22 21:17:46 +0200

    Add files via upload
    Committing Main class.

---
+++ src/com/mystreams/Main.java
@@ -0,0 +1,32 @@
+/**
+ *  Java program to create a stream of file.
+ */
+
+package com.mystreams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * com.mystreams.Main class.
+ */
+
+public class Main {
+
+    // JVM entry point.
+    public static void main(String[] args) throws IOException {
+
+        // Path to file.
+        Path path = Paths.get("src/file.txt");
+
+        // Creating a stream with cgaracters.
+        Stream<String> streamOfString = Files.lines(path);
+
+        // Printing streamOfString to console.
+        streamOfString.forEach(System.out::println); // Output: Hello World.
+
+    }
+}
